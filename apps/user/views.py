9bbdc565dff97ce8dbce68746655a8aefb7a40d0
Ragas Imger <regmis851@gmail.com>
2022-07-17 23:19:35 +0545

    Leaving social auth as of now due to lack of standout frontend

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,9 +1,6 @@
 '''
     From Local 
 '''
-from apps.user.process_social.serializers import (
-    FacebookSocialAuthSerializer, GoogleSocialAuthSerializer, TwitterAuthSerializer
-)
 from apps.user.serializers import(
     UserRegisterSerializer, AdminLevelUserSerializer, UserUpdateSerializer, VerifyOtpSerializer, ResendOtpSerializer
 )
@@ -22,11 +19,6 @@ from django.contrib.auth import get_user_model
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.mixins import CreateModelMixin
 from rest_framework.permissions import IsAuthenticated
-from dj_rest_auth.registration.views import SocialLoginView
-from allauth.socialaccount.providers.facebook.views import FacebookOAuth2Adapter
-from allauth.socialaccount.providers.twitter.views import TwitterOAuthAdapter
-from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
-from dj_rest_auth.social_serializers import TwitterLoginSerializer
 
 ###### Package Imports End
 
@@ -68,16 +60,3 @@ class AdminLevelUserViewSet(viewsets.ModelViewSet):
     search_fields = ("username",)
     filter_backends = [filters.SearchFilter, DjangoFilterBackend]
     filterset_fields = ("username", "email", "first_name",)
-
-
-class FacebookLoginSignUpView(SocialLoginView):
-    serializer_class = FacebookSocialAuthSerializer
-    adapter_class = FacebookOAuth2Adapter
-
-class TwitterLoginSignUpView(SocialLoginView):
-    serializer_class = TwitterAuthSerializer
-    adapter_class = TwitterOAuthAdapter
-
-class GoogleLoginSignUpView(SocialLoginView):
-    serializer_class = GoogleSocialAuthSerializer
-    adapter_class = GoogleOAuth2Adapter
